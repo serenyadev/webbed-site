b866f4987ef54492a5c748081bbae25bfed23269
sery <sery@sappho.company>
2024-11-15 16:14:31 +0100

    god save me for i have wrote js

---
+++ scripts/fancy.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    for (let button in document.getElementsByClassName("slideshow_next")) {
+        button.addEventListener("click", event => {
+            let slideshow = document.getElementById(button.getAttribute("target"));
+            let active, next = null, nextActive;
+            for(let element in slideshow.children) {
+                if(element.classList.contains("active") && !nextActive) {
+                    active = element;
+                    nextActive = true;
+                } else if(nextActive) {
+                    next = element;
+                    break;
+                }
+            }
+            if(next == null) next = slideshow.children[0];
+            slideshowNext(slideshow, active, next)
+        });
+    }
+});
+
+function slideshowNext(slideshow, current, next) {
+    current.classList.remove("active");
+    next.classList.add("active");
+}
